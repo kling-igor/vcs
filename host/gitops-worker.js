d16b3cadeafc6b20a68a5de5f02949995efdf14d
Igor Kling <klingiv@altarix.ru>
2019-09-29 22:47:06 +0400

    fetch operation - WIP

--- host/gitops-worker.js
+++ host/gitops-worker.js
@@ -1,4 +1,4 @@
-import { openRepository, log } from './gitops'
+import { openRepository, log, fetch } from './gitops'
 
 const [command, ...args] = process.argv.slice(2)
 
@@ -9,6 +9,13 @@ async function gitlog(repoPath) {
   return log(repo)
 }
 
+async function fetch(repoPath, remoteName, userName, password) {
+  if (!repoPath) throw new Error('Repositiry path not specified')
+
+  const repo = await openRepository(repoPath)
+  return fetch(repo, remoteName, userName, password)
+}
+
 // и другие операции
 
 ;(async () => {
@@ -16,6 +23,9 @@ async function gitlog(repoPath) {
     if (command === 'gitlog') {
       const history = await gitlog(...args)
       process.send({ log: history })
+    } else if (command === 'fetch') {
+      await fetch(...args)
+      process.send({ fetch: 'done' })
     }
   } catch (e) {
     process.send({ error: e.message })
