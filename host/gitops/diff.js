8536244927e13c591b4bbbd9cbbbc077c2ff320d
Igor Kling <klingiv@altarix.ru>
2019-10-14 13:25:48 +0400

    reafactor diff operations - WIP - main.js to be  improved accordingly

--- host/gitops/diff.js
+++ host/gitops/diff.js
@@ -3,36 +3,12 @@ import { resolve } from 'path'
 import { readFile } from 'fs-extra'
 
 /**
- *
- * @param {*} repo
- * @param {*} sha
- * @param {*} filePath
+ * @param {nodegit.Repository} repo
+ * @param {String} sha
+ * @param {String} filePath
  * @returns {Buffer}
  */
-async function getParentCommitFileContent(repo, sha, filePath) {
-  try {
-    const oid = nodegit.Oid.fromString(sha)
-    const commit = await repo.getCommit(oid)
-    const [parentSha] = commit.parents()
-    if (parentSha) {
-      const parentCommit = await repo.getCommit(parentSha)
-      const originalEntry = await parentCommit.getEntry(filePath)
-
-      if (originalEntry && originalEntry.isFile()) {
-        return await originalEntry.getBlob()
-      }
-    }
-  } catch (e) {}
-}
-
-/**
- *
- * @param {*} repo
- * @param {*} sha
- * @param {*} filePath
- * @returns {Buffer}
- */
-async function getCommitedFileContent(repo, sha, filePath) {
+async function getCommittedFileContent(repo, sha, filePath) {
   try {
     const oid = nodegit.Oid.fromString(sha)
     const commit = await repo.getCommit(oid)
@@ -48,24 +24,39 @@ async function getCommitedFileContent(repo, sha, filePath) {
 }
 
 /**
+ * @param {nodegit.Repository} repo
+ * @param {String} filePath
+ * @returns {Buffer}
+ */
+async function getIndexedFileContent(repo, filePath) {
+  const index = await repo.index()
+  const entry = index.entries().find(item => item.path === filePath)
+  if (entry) {
+    const blob = await nodegit.Blob.lookup(repo, entry.id)
+    return blob.content()
+  }
+}
+
+/**
  * File current and parent versions
- * @param {Repository} repo
+ * @param {nodegit.Repository} repo
  * @param {String} sha
  * @param {String} filePath - file relative path to repository
+ * @returns {{originalContent: String, modifiedContent:String}}
  */
 export async function fileDiffToParent(repo, sha, filePath) {
   let originalContent = ''
   let modifiedContent = ''
 
   try {
-    originalContent = (await getCommitedFileContent(repo, sha, filePath)).toString()
+    originalContent = (await getCommittedFileContent(repo, sha, filePath)).toString()
 
     const oid = nodegit.Oid.fromString(sha)
     const commit = await repo.getCommit(oid)
 
     const [parentSha] = commit.parents()
     if (parentSha) {
-      modifiedEntry = (await getCommitedFileContent(repo, parentSha, filePath)).toString()
+      modifiedEntry = (await getCommittedFileContent(repo, parentSha, filePath)).toString()
     }
     return {
       originalContent,
@@ -108,6 +99,52 @@ export async function fileDiffToParent(repo, sha, filePath) {
   // return { details: 'ERROR!!!!' }
 }
 
+/**
+ *
+ * @param {nodegit.Repository} repo
+ * @param {String} filePath
+ * @returns {Buffer}
+ */
+export async function getMineFileContent(repo, filePath) {
+  if (!repo.isMerging()) return
+  // const headCommit = await repo.getHeadCommit()
+  // const mineEntry = await headCommit.getEntry(filePath)
+  // if (mineEntry && mineEntry.isFile()) {
+  //   return (await mineEntry.getBlob()).toString()
+  // }
+
+  const index = await repo.index()
+  const conflict = await index.conflictGet(filePath)
+  return await repo.getBlob(conflict.our_out.id)
+  // return mineBlob.toString()
+}
+
+/**
+ * @param {nodegit.Repository} repo
+ * @param {String} filePath
+ * @returns {Buffer}
+ */
+export async function getTheirsFileContent(repo, filePath) {
+  if (!repo.isMerging()) return
+
+  // const theirsCommit = await repo.getBranchCommit('MERGE_HEAD')
+  // const theirsEntry = await theirsCommit.getEntry(filePath)
+  // if (theirsEntry && theirsEntry.isFile()) {
+  //   return (await theirsEntry.getBlob()).toString()
+  // }
+
+  const index = await repo.index()
+  const conflict = await index.conflictGet(filePath)
+  return await repo.getBlob(conflict.their_out.id)
+  // return theirsBlob.toString()
+}
+
+/**
+ * @param {nodegit.Repository} repo
+ * @param {String} projectPath
+ * @param {String} filePath
+ * @returns {{originalContent:String, modifiedContent:String}}
+ */
 export async function changedFileDiffToIndex(repo, projectPath, filePath) {
   // TODO: а если нет еще коммитов ?
 
@@ -116,13 +153,15 @@ export async function changedFileDiffToIndex(repo, projectPath, filePath) {
   let originalContent = ''
   let modifiedContent = ''
   try {
-    const index = await repo.index()
-    const entry = index.entries().find(item => item.path === filePath)
-    if (entry) {
-      const blob = await nodegit.Blob.lookup(repo, entry.id)
-      const buffer = blob.content()
-      originalContent = buffer.toString()
-    }
+    // const index = await repo.index()
+    // const entry = index.entries().find(item => item.path === filePath)
+    // if (entry) {
+    //   const blob = await nodegit.Blob.lookup(repo, entry.id)
+    //   const buffer = blob.content()
+    //   originalContent = buffer.toString()
+    // }
+
+    originalContent = (await getIndexedFileContent(repo, filePath)).toString()
 
     modifiedContent = await readFile(resolve(projectPath, filePath), { encoding: 'utf-8' })
 
@@ -135,6 +174,11 @@ export async function changedFileDiffToIndex(repo, projectPath, filePath) {
   }
 }
 
+/**
+ * @param {nodegit.Repository} repo
+ * @param {String} filePath
+ * @returns {{originalContent:String, modifiedContent:String}}
+ */
 export async function stagedFileDiffToHead(repo, filePath) {
   let originalContent = ''
   let modifiedContent = ''
@@ -143,22 +187,25 @@ export async function stagedFileDiffToHead(repo, filePath) {
     try {
       const headCommit = await repo.getHeadCommit()
       if (headCommit) {
-        const originalEntry = await headCommit.getEntry(filePath)
-        if (originalEntry && originalEntry.isFile()) {
-          originalContent = (await originalEntry.getBlob()).toString()
-        }
+        // const originalEntry = await headCommit.getEntry(filePath)
+        // if (originalEntry && originalEntry.isFile()) {
+        //   originalContent = (await originalEntry.getBlob()).toString()
+        // }
+        originalContent = (await getCommittedFileContent(repo, headCommit.sha(), filePath)).toString()
       }
     } catch (e) {
       console.error('!!!!:', e)
     }
 
-    const index = await repo.index()
-    const entry = index.entries().find(item => item.path === filePath)
-    if (entry) {
-      const blob = await nodegit.Blob.lookup(repo, entry.id)
-      const buffer = blob.content()
-      modifiedContent = buffer.toString()
-    }
+    // const index = await repo.index()
+    // const entry = index.entries().find(item => item.path === filePath)
+    // if (entry) {
+    //   const blob = await nodegit.Blob.lookup(repo, entry.id)
+    //   const buffer = blob.content()
+    //   modifiedContent = buffer.toString()
+    // }
+
+    modifiedContent = (await getIndexedFileContent(repo, filePath)).toString()
 
     return {
       originalContent,
@@ -167,32 +214,4 @@ export async function stagedFileDiffToHead(repo, filePath) {
   } catch (e) {
     console.log(e)
   }
-}
-
-export async function getMineFileContent(repo, filePath) {
-  // const headCommit = await repo.getHeadCommit()
-  // const mineEntry = await headCommit.getEntry(filePath)
-  // if (mineEntry && mineEntry.isFile()) {
-  //   return (await mineEntry.getBlob()).toString()
-  // }
-
-  const index = await repo.index()
-  const conflict = await index.conflictGet(filePath)
-  const mineBlob = await repo.getBlob(conflict.our_out.id)
-  return mineBlob.toString()
-}
-
-export async function getTheirsFileContent(repo, filePath) {
-  if (!repo.isMerging()) return
-
-  // const theirsCommit = await repo.getBranchCommit('MERGE_HEAD')
-  // const theirsEntry = await theirsCommit.getEntry(filePath)
-  // if (theirsEntry && theirsEntry.isFile()) {
-  //   return (await theirsEntry.getBlob()).toString()
-  // }
-
-  const index = await repo.index()
-  const conflict = await index.conflictGet(filePath)
-  const theirsBlob = await repo.getBlob(conflict.their_out.id)
-  return theirsBlob.toString()
 }
